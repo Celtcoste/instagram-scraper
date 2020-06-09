3c1be015220e734a9eae236ab7196af350b476d8
Alexandre Ablon <ab@fendcer.com>
2020-06-09 13:22:19 +0200

    change setup and remove joetag

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 from pathlib import Path
 
 setuptools.setup(
-    name="igramscraper",
-    version="0.3.5",
+    name="instagram-scraper",
+    version="0.1",
     description=('scrapes medias, likes, followers, tags and all metadata'),
-    long_description=Path("README.md").read_text(),
+    long_description='forked version of igscraper : https://github.com/realsirjoe/instagram-scraper',
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     license="MIT",
     maintainer="realsirjoe, leungwaiban",
     author='realsirjoe, leungwaiban',
-    url='https://github.com/realsirjoe/instagram-scraper',
+    url='https://github.com/Celtcoste/instagram-scraper',
     install_requires=[
         'requests>=2.21.0',
         'python-slugify==3.0.2'
